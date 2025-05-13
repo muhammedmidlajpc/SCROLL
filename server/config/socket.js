a381b65c205883942ff6bf7498dd732b1817622d
Muhammed Midlaj PC <95988724+muhammedmidlajpc@users.noreply.github.com>
2025-05-13 18:54:06 +0530

    Update socket.js

--- server/config/socket.js
+++ server/config/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"]
+    origin: ["https://scroll-front-end.onrender.com"]
     // origin: ["https://scroll542.netlify.app"] 
   }
 });
